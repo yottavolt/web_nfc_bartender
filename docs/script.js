c5f35f6d341ab91c95222237e0829d02f32b369d
yottavolt <jonahdistl@gmail.com>
2026-08-08 17:20:37 +0200

    changed syntax

--- docs/script.js
+++ docs/script.js
@@ -380,7 +380,7 @@ const DrinkButtons = [
   {
     id: 1,
     imageUrl: 'img/drink11.png',
-    payload: '//<01>',      
+    payload: '<01>',      
     name: 'Tequilla Sunrise',        
     recipe1: "<CFGD><01><00210304><00FF00FF00><01020304><00000000>><01020304><00000000></>",
     comment: "Enjoy your drink :)"
@@ -388,7 +388,7 @@ const DrinkButtons = [
   {
     id: 2,
     imageUrl: 'img/drink1.png',
-    payload: '//<xx>',         
+    payload: '<xx>',         
     name: 'Sex on the Beach', 
     recipe1: "<CFGD><02><01020304><00FF00FF00><01020304><00000000>><01020304><00000000></>",
     comment: "Tits :)"    
@@ -396,14 +396,14 @@ const DrinkButtons = [
   {
     id: 3,
     imageUrl: 'img/drink9.png',
-    payload: 'blablalb',
+    payload: '<02>',
     name: 'Blue Lagoon',
     recipe1: "<CFGD><03><01020304><00FF00FF00><01020304><00000000>><01020304><00000000></>",
   },
   {
     id: 4,
     imageUrl: 'img/drink10.png',
-    payload: '<xx>',
+    payload: '<03>',
     name: 'Mai Tai',
     recipe1: "<CFGD><04><01020304><00FF00FF00><01020304><00000000>><01020304><00000000></>",
   },
